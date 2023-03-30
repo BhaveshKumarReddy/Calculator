89947eb0510ed2e0cc10df45d1cdf8c43fde6a6d
Pavith <35229068+Ram-Pavith@users.noreply.github.com>
2023-03-30 10:39:40 +0530

    Added Multiplication Functionality

---
+++ Multiply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Multiply
+    {
+        public static int mul(int a, int b) { return a * b; }
+        public static float mul(float a, float b) { return a * b; }
+        public static double mul(double a, double b) { return a * b; }
+
+    }
+}
